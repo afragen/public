eb0cbff829f768175563f431511c9a068acac45c
Andy Fragen <andy@thefragens.com>
2024-03-09 12:00:41 -0800

    add $cookies back

--- wp-admin/includes/class-wp-automatic-updater.php
+++ wp-admin/includes/class-wp-automatic-updater.php
@@ -1705,6 +1705,7 @@ Thanks! -- The WordPress Team"
 
 		$scrape_key    = md5( $upgrading );
 		$scrape_nonce  = (string) $upgrading;
+		$cookies       = wp_unslash( $_COOKIE );
 		$transient     = 'scrape_key_' . $scrape_key;
 		set_transient( $transient, $scrape_nonce, 30 );
 
@@ -1732,7 +1733,7 @@ Thanks! -- The WordPress Team"
 		$needle_start = "###### wp_scraping_result_start:$scrape_key ######";
 		$needle_end   = "###### wp_scraping_result_end:$scrape_key ######";
 		$url          = add_query_arg( $scrape_params, home_url( '/' ) );
-		$response     = wp_remote_get( $url, compact( 'headers', 'timeout', 'sslverify' ) );
+		$response     = wp_remote_get( $url, compact( 'cookies', 'headers', 'timeout', 'sslverify' ) );
 
 		// If this outputs `true` in the log, it means there were no fatal errors detected.
 		error_log( var_export( substr( $response['body'], strpos( $response['body'], '###### wp_scraping_result_start:' ) ), true ) );
